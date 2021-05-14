3690123ad0033187149fcc565e2177bfbd1f5b2b
saksham-kapoor <sakshamkapoor1729@gmail.com>
2021-05-14 17:55:49 +0530

    improve preorder traversal bt

--- Trees/preorder-traversal.cpp
+++ Trees/preorder-traversal.cpp
@@ -1,29 +1,63 @@
-// ITERATIVE SOLUTION
 class Solution
 {
 public:
     vector<int> preorderTraversal(TreeNode *root)
     {
         vector<int> ans;
-        stack<pair<TreeNode *, int>> s;
-        s.push({root, 0});
 
-        while (!s.empty())
+        stack<pair<TreeNode *, int>> st;
+        st.push({root, 0});
+
+        while (st.size())
         {
-            pair<TreeNode *, int> curr = s.top();
-            s.pop();
+            auto [node, level] = st.top();
+            st.pop();
 
-            if (curr.first == nullptr || curr.second == 3)
+            if (node == nullptr || level == 3)
                 continue;
 
-            s.push({curr.first, curr.second + 1});
+            st.push({node, level + 1});
 
-            if (curr.second == 0)
-                ans.push_back(curr.first->val);
-            else if (curr.second == 1)
-                s.push({curr.first->left, 0});
-            else if (curr.second == 2)
-                s.push({curr.first->right, 0});
+            if (level == 0)
+            {
+                ans.push_back(node->val);
+            }
+            else if (level == 1)
+            {
+                st.push({node->left, 0});
+            }
+            else if (level == 2)
+            {
+                st.push({node->right, 0});
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Simple stack solution
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+
+        stack<TreeNode *> st;
+        st.push(root);
+
+        while (st.size())
+        {
+            TreeNode *node = st.top();
+            st.pop();
+
+            if (node == nullptr)
+                continue;
+
+            ans.push_back(node->val); // print
+            st.push(node->right);
+            st.push(node->left);
         }
 
         return ans;
