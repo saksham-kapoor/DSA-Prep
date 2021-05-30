b018cf3f6ab05009df4341af37b740624c7deba5
saksham-kapoor <sakshamkapoor1729@gmail.com>
2021-05-30 20:54:25 +0530

    min path sum - space optimization

--- DP-patterns/Optimize-Path-To-Reach-Target/2-min-path-sum.cpp
+++ DP-patterns/Optimize-Path-To-Reach-Target/2-min-path-sum.cpp
@@ -36,3 +36,26 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// SPACE OPTIMIZATION
+// SPACE OPTIMIZATION
+class Solution
+{
+public:
+    int minPathSum(vector<vector<int>> &g)
+    {
+        int m = g.size(), n = g[0].size();
+
+        for (int i = 1; i < n; ++i)
+            g[0][i] += g[0][i - 1];
+
+        for (int i = 1; i < m; ++i)
+            g[i][0] += g[i - 1][0];
+
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                g[i][j] += min(g[i - 1][j], g[i][j - 1]);
+
+        return g[m - 1][n - 1];
+    }
+};
