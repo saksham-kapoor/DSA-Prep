df377ca8e661045f67f19091beceb135e1c46332
saksham-kapoor <sakshamkapoor1729@gmail.com>
2021-06-13 20:34:53 +0530

    floyd warshall correction!

--- Graph-patterns/shortest-path/floyd-warshall.cpp
+++ Graph-patterns/shortest-path/floyd-warshall.cpp
@@ -9,10 +9,16 @@ public:
     void shortest_distance(vector<vector<int>> &d)
     {
         int n = d.size();
+
+        // IMP. INITIALIZE ALL '-1' VALUES TO INT_MAX
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                d[i][j] = d[i][j] == -1 ? INT_MAX : d[i][j];
+
         for (int k = 0; k < n; ++k)         // via node
             for (int i = 0; i < n; ++i)     // source node
                 for (int j = 0; j < n; ++j) // destination node
-                    if (d[i][k] == -1 || d[k][j] == -1)
+                    if (d[i][k] == INT_MAX || d[k][j] == INT_MAX)
                         continue;
                     else if (d[i][k] + d[k][j] < d[i][j])
                         d[i][j] = d[i][k] + d[k][j];
